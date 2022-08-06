cbf968d8ff2ef126e76b62a8b4841b9d098bd184
Documenter.jl <documenter@juliadocs.github.io>
2022-08-06 00:33:35 +0000

    build based on 7c8c443

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CifAPI","category":"page"},{"location":"#CifAPI","page":"Home","title":"CifAPI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CifAPI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CifAPI]","category":"page"}]
+}
